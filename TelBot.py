eac7229e12c24c909fbef713d1425cc8c9c3789a
arsentiisv <senia071205@gmail.com>
2025-06-02 15:16:39 +0300

    Final version of bot

--- TelBot.py
+++ TelBot.py
@@ -3,20 +3,23 @@ import pandas as pd
 from datetime import datetime, timedelta
 import plotly.graph_objects as go
 import telebot
+from telebot import types
 from tinkoff.invest import Client, CandleInterval
 from RefTrain import StockPredictorApp
-from SentimentPart import Parser,SentiAn
+from SentimentPart import Parser, SentiAn
+from gpt1 import GPTAnalyzer
 import time
 import threading
 
-BOT_TOKEN     = '7580262489:AAHCy1YK_WT6iriTBEyTVD5P9uZzu-yP_kw'
-TINKOFF_TOKEN = 't.wvc9ZeUlJjxD_HBUE0qGA95v2M_df56u36SGiR0KkexEgx7TtTA1CYn5mdvrwF0X8jE5oWbGb4rPLPU-VKJ6LA'
+BOT_TOKEN = 'BToken'
+TINKOFF_TOKEN = 'TToken'
+GPT_API_KEY = "GToken"
 
 Ml = StockPredictorApp()
-parse=Parser()
-sentic=SentiAn()
+parse = Parser()
+sentic = SentiAn()
 bot = telebot.TeleBot(BOT_TOKEN)
-user_data = {}  # { chat_id: { 'vrFi': DataFrame, 'tiki': str, 'figi': str, 'data_df': DataFrame, 'kon': datetime, 'na': datetime, 'search':str } }
+user_data = {}  # { chat_id: { 'vrFi': DataFrame, 'tiki': str, 'figi': str, 'data_df': DataFrame, 'kon': datetime, 'na': datetime, 'search':str, 'username':str, 'first':str, 'last':str } }
 
 
 def simS(str1, str2, zn):
@@ -45,8 +48,8 @@ def naiFi(name):
         vr = {'ticker': [], 'figi': [], 'name': [], 'type': []}
         for t in ['shares', 'bonds', 'etfs', 'currencies']:
             for item in getattr(cl.instruments, t)().instruments:
-                if simS(name, item.name,  min(len(name), len(item.name)) // 1.75) or \
-                   simS(name, item.ticker, min(len(name), len(item.ticker)) // 1.75):
+                if simS(name, item.name, min(len(name), len(item.name)) // 1.75) or \
+                        simS(name, item.ticker, min(len(name), len(item.ticker)) // 1.75):
                     vr['ticker'].append(item.ticker)
                     vr['figi'].append(item.figi)
                     vr['type'].append(t)
@@ -61,13 +64,13 @@ def perd(p):
 def svechkiT(figi):
     kon = datetime.now() + timedelta(days=1)
     na = kon - timedelta(days=10 * 365)
-    da = { 'date': [], 'Open': [], 'High': [], 'Low': [], 'Close': [], 'Volume': [] }
+    da = {'date': [], 'Open': [], 'High': [], 'Low': [], 'Close': [], 'Volume': []}
     with Client(TINKOFF_TOKEN) as cl:
         for candle in cl.get_all_candles(
-            figi=figi,
-            from_=na,
-            to=kon,
-            interval=CandleInterval.CANDLE_INTERVAL_DAY
+                figi=figi,
+                from_=na,
+                to=kon,
+                interval=CandleInterval.CANDLE_INTERVAL_DAY
         ):
             da['date'].append(str(candle.time + timedelta(hours=3))[:19])
             da['Open'].append(perd(candle.open))
@@ -80,7 +83,8 @@ def svechkiT(figi):
 
 
 def animate_loading(bot, chat_id, message_id, stop_event):
-    frames = ["⏳ Loading", "⏳ Loading.", "⏳ Loading..", "⏳ Loading..."]
+    # Кадры в анимации
+    frames = ["⏳ Loading", "⏳ Loading .", "⏳ Loading . .", "⏳ Loading . . ."]
     i = 0
     while not stop_event.is_set():
         frame = frames[i % len(frames)]
@@ -89,14 +93,15 @@ def animate_loading(bot, chat_id, message_id, stop_event):
         except Exception:
             pass
         i += 1
-        #скорость обновления
+        # скорость обновления
         time.sleep(5)
 
+
 def send_candle_chart(chat_id: int) -> None:
-    # 1. Отправляем первое сообщение “Loading”
+    # Первое сообщение
     loading_msg = bot.send_message(chat_id=chat_id, text="⏳ Loading")
 
-    # 2. Запускаем поток анимации
+    # Анимация
     stop_event = threading.Event()
     anim_thread = threading.Thread(
         target=animate_loading,
@@ -111,6 +116,12 @@ def send_candle_chart(chat_id: int) -> None:
 
         display_df_formatted = s[0][-40:]
         mae, rmse, mape, r2 = s[1], s[2], s[3], s[4]
+        user_data[chat_id]['metrics'] = {
+            'mae': mae,
+            'rmse': rmse,
+            'mape': mape,
+            'r2': r2
+        }
 
         fig = go.Figure()
         fig.add_trace(go.Scatter(
@@ -121,7 +132,7 @@ def send_candle_chart(chat_id: int) -> None:
         ))
         test_df = display_df_formatted[
             (display_df_formatted['is_forecast'] == True) & (display_df_formatted['lower_ci'].isna())
-        ]
+            ]
         fig.add_trace(go.Scatter(
             x=test_df.index,
             y=test_df['forecast'],
@@ -147,7 +158,7 @@ def send_candle_chart(chat_id: int) -> None:
         metrics_text = f"MAE: {mae:.2f} │ RMSE: {rmse:.2f} │ MAPE: {mape:.2f}% │ R²: {r2:.4f}"
         fig.update_layout(
             title=f"Forecast of {tiki} with confidence intervals",
-            xaxis_title="Date",
+            # xaxis_title="Date",
             yaxis_title="Close Price",
             template="plotly_white",
             annotations=[dict(
@@ -165,18 +176,19 @@ def send_candle_chart(chat_id: int) -> None:
             legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5)
         )
 
-        # Сохраняем и отправляем изображение
+        # Сохраняем и отправляем изображение, временная папка где они лежат
         temppics_dir = os.path.join(os.getcwd(), 'temppics')
         os.makedirs(temppics_dir, exist_ok=True)
         image_path = os.path.join(temppics_dir, f'candlestick_chart_{chat_id}.png')
+        user_data[chat_id]['path'] = image_path
         fig.write_image(image_path, engine='kaleido')
-        #Чистим путь для подключения чата это надо перенести после чата
+
+        # Отправляем фото
         with open(image_path, 'rb') as photo:
             bot.send_photo(chat_id, photo)
-        os.remove(image_path)
 
     finally:
-        # 4. Тормозим и итоговое
+        # Останавливаем анимацию
         stop_event.set()
         anim_thread.join()
 
@@ -185,102 +197,86 @@ def send_candle_chart(chat_id: int) -> None:
         except Exception:
             pass
 
-# def send_candle_chart(chat_id: int) -> None:
-#     data = user_data[chat_id]['data_df']
-#     tiki = user_data[chat_id]['tiki']
-#     s = Ml.run(data)
-#     display_df_formatted = s[0][-40:]
-#     mae, rmse, mape, r2 = s[1], s[2], s[3], s[4]
-#
-#     fig = go.Figure()
-#     fig.add_trace(go.Scatter(
-#         x=display_df_formatted.index,
-#         y=display_df_formatted['close'],
-#         name="Historic prices",
-#         line=dict(color="blue", width=2)
-#     ))
-#     test_df = display_df_formatted[
-#         (display_df_formatted['is_forecast'] == True) & (display_df_formatted['lower_ci'].isna())
-#     ]
-#     fig.add_trace(go.Scatter(
-#         x=test_df.index,
-#         y=test_df['forecast'],
-#         name="Test forecast",
-#         line=dict(color="orange", width=2)
-#     ))
-#     future_df = display_df_formatted[display_df_formatted['lower_ci'].notna()]
-#     fig.add_trace(go.Scatter(
-#         x=future_df.index,
-#         y=future_df['forecast'],
-#         name="Future forecast",
-#         line=dict(color="green", width=3)
-#     ))
-#     fig.add_trace(go.Scatter(
-#         x=future_df.index.tolist() + future_df.index.tolist()[::-1],
-#         y=future_df['upper_ci'].tolist() + future_df['lower_ci'].tolist()[::-1],
-#         fill='toself',
-#         fillcolor='rgba(0,255,0,0.2)',
-#         line=dict(color='rgba(255,255,255,0)'),
-#         name='80% Confidence interval'
-#     ))
-#
-#     metrics_text = f"MAE: {mae:.2f} │ RMSE: {rmse:.2f} │ MAPE: {mape:.2f}% │ R²: {r2:.4f}"
-#     fig.update_layout(
-#         title=f"Forecast of {tiki} with confidence intervals",
-#         xaxis_title="Date",
-#         yaxis_title="Close Price",
-#         template="plotly_white",
-#         annotations=[dict(
-#             text=metrics_text,
-#             xref="paper", yref="paper",
-#             x=0.5, y=-0.15,
-#             xanchor='center', yanchor='top',
-#             showarrow=False,
-#             font=dict(size=12, color="black"),
-#             bgcolor="rgba(255,255,255,0.8)",
-#             bordercolor="rgba(0,0,0,0.1)",
-#             borderwidth=1
-#         )],
-#         margin=dict(b=100),
-#         legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5)
-#     )
-#
-#     temppics_dir = os.path.join(os.getcwd(), 'temppics')
-#     os.makedirs(temppics_dir, exist_ok=True)
-#     image_path = os.path.join(temppics_dir, f'candlestick_chart_{chat_id}.png')
-#     fig.write_image(image_path, engine='kaleido')
-#
-#     with open(image_path, 'rb') as photo:
-#         bot.send_photo(chat_id, photo)
-#     os.remove(image_path)
 
-
+# Команда /start обязательная
 @bot.message_handler(commands=['start'])
 def start(msg):
     chat_id = msg.chat.id
-    user_data[chat_id] = {}
+    user_data[chat_id] = {
+        'username': msg.from_user.username,
+        'first': msg.from_user.first_name,
+        'last': msg.from_user.last_name,
+    }
     bot.send_message(chat_id, "Enter the name of the company\nor it's ticker:")
 
 
-@bot.message_handler(func=lambda m: True)
+# Команда /Describe опциональная
+@bot.message_handler(commands=['Describe'])
+def llm_message(msg):
+    chat_id = msg.chat.id
+    gpt_analyzer = GPTAnalyzer(GPT_API_KEY)
+    # Проверка не пустой ли чат
+    if chat_id not in user_data or 'search' not in user_data[chat_id]:
+        bot.send_message(chat_id, "Please first search for a company using /start")
+        return
+
+    kon = datetime.now()
+    na = kon - timedelta(days=40)
+    kon = kon.strftime('%Y-%m-%d')
+    na = na.strftime('%Y-%m-%d')
+    txt = user_data[chat_id]['search']
+    tiki = user_data[chat_id]['tiki']
+
+    image_path = user_data[chat_id].get('path')
+
+    # Парсинг новостей
+    tabl = parse.SAn(txt, na, kon)
+    tabl = sentic.fin(tabl)
+    metrics = user_data[chat_id]['metrics']
+    if image_path and os.path.exists(image_path):
+        gpt_result = gpt_analyzer.analyze(
+            image_path=image_path,
+            sentiment_df=tabl,
+            ticker=tiki,
+            metrics=metrics
+        )
+        bot.send_message(chat_id, f"GPT analysis:\n{gpt_result}")
+
+        # Удаляем временный файл из папка
+        if image_path and os.path.exists(image_path):
+            os.remove(image_path)
+
+    # Небольшой отчёт в консоль
+    print(
+        f'Запрос - {txt}, Пользователь - {user_data[chat_id]["username"]}  {user_data[chat_id]["first"]}  {user_data[chat_id]["last"]}')
+    # Перезапуск бота после ллмки
+    bot.send_message(chat_id, "To restart press /start")
+
+
+def predfinal_message(chat_id: int):
+    bot.send_message(chat_id, "Graph is done\nfor llm press /Describe")
+
+
+# Нужно доработать, все остальные сообщения принимаются через шаблоны
+@bot.message_handler(func=lambda message: not message.text.startswith('/'))
 def get_instrument(msg):
     chat_id, txt = msg.chat.id, msg.text.strip()
     try:
         vrFi = naiFi(txt)
         if vrFi.empty:
-            bot.send_message(chat_id, "Не удалось найти инструмент. Попробуйте снова.")
+            bot.send_message(chat_id, "Try another index:")
             return
         user_data[chat_id]['vrFi'] = vrFi
         user_data[chat_id]['search'] = txt
 
         listing = "\n".join([
-            f"{i+1}. {row['name']} — {row['ticker']}"
+            f"{i + 1}. {row['name']} — {row['ticker']}"
             for i, row in vrFi.iterrows()
         ])
-        bot.send_message(chat_id, f"Similar companies:\n{listing}\n\nWrite the number:")
+        bot.send_message(chat_id, f"Similar companies:\n\n{listing}\n\nWrite the number:")
         bot.register_next_step_handler(msg, select_instrument)
     except Exception as e:
-        bot.send_message(chat_id, f"Ошибка при поиске: {e}")
+        bot.send_message(chat_id, f"Error: {e}")
 
 
 def select_instrument(msg):
@@ -288,7 +284,7 @@ def select_instrument(msg):
     try:
         text = msg.text.strip()
         if not text.isdigit():
-            bot.send_message(chat_id, "Пожалуйста, введите число, соответствующее выбору.")
+            bot.send_message(chat_id, "Enter index from the table:")
             bot.register_next_step_handler(msg, select_instrument)
             return
 
@@ -299,9 +295,9 @@ def select_instrument(msg):
             bot.register_next_step_handler(msg, select_instrument)
             return
 
-        chosen_row = vrFi.iloc[idx]
-        figi = chosen_row['figi']
-        tiki = chosen_row['ticker']
+        row = vrFi.iloc[idx]
+        figi = row['figi']
+        tiki = row['ticker']
 
         user_data[chat_id]['figi'] = figi
         user_data[chat_id]['tiki'] = tiki
@@ -316,22 +312,11 @@ def select_instrument(msg):
         user_data[chat_id]['na'] = na
 
         send_candle_chart(chat_id)
-        final_message(chat_id)
+        predfinal_message(chat_id)
 
     except Exception as e:
-        bot.send_message(chat_id, f"Ошибка при выборе инструмента: {e}")
+        bot.send_message(chat_id, f"Error: {e}")
         bot.register_next_step_handler(msg, select_instrument)
-
-
-def final_message(chat_id: int):
-    bot.send_message(chat_id, "Graph done. Restart command: /start")
-    na=user_data[chat_id]['na'].strftime('%Y-%m-%d')
-    kon = user_data[chat_id]['kon'].strftime('%Y-%m-%d')
-    txt=user_data[chat_id]['search']
-    tabl = parse.SAn(txt, na, kon)
-    tabl = sentic.fin(tabl)
-    print(tabl)
-    print(txt)
 
 
 bot.polling(none_stop=True)
